361b4ae3051ae46f041d2bc0233b5f8a7d3b3f16
G. Weibell <13382354+weibell@users.noreply.github.com>
2023-12-04 00:33:37 +0100

    Correct solution

--- day01/part2.py
+++ day01/part2.py
@@ -10,12 +10,15 @@ mappings = {
   '7': 7, '8': 8, '9': 9
 }
 
+def positions_and_values(line: str, from_left: bool):
+  line_find = line.find if from_left else line.rfind
+  return [(line_find(digit), value) for digit, value in mappings.items() if line_find(digit) != -1]
+
 calibration_values = []
 for line in input:
-  positions_and_values = sorted([(line.find(digit), value) for digit, value in mappings.items()
-                          if line.find(digit) != -1])
-  first_value = positions_and_values[0][1]
-  last_value = positions_and_values[-1][1]
+  first_value = sorted(positions_and_values(line, from_left=True))[0][1]
+  last_value = sorted(positions_and_values(line, from_left=False))[-1][1]
+
   calibration_values.append(int(str(first_value) + str(last_value)))
 
 print(sum(calibration_values))
