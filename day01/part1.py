76a5d3269387028dbef7eaf733fe9960bb9bf144
weibell <13382354+weibell@users.noreply.github.com>
2023-12-06 00:08:11 +0100

    Fix formatting

--- day01/part1.py
+++ day01/part1.py
@@ -1,9 +1,9 @@
 with open('input.txt', 'r') as f:
-  input = f.read().splitlines()
+    input_data = f.read().splitlines()
 
 calibration_values = []
-for line in input:
-  digits = [char for char in line if char in '0123456789']
-  calibration_values.append(int(digits[0] + digits[-1]))
+for line in input_data:
+    digits = [char for char in line if char in '0123456789']
+    calibration_values.append(int(digits[0] + digits[-1]))
 
 print(sum(calibration_values))
--- day01/part2.py
+++ day01/part2.py
@@ -1,24 +1,27 @@
+
+def positions_and_values(line: str, dir: str) -> list[tuple[int, int]]:
+    # returns a list of (index, value)
+    mappings = {
+        '1': 1, 'one': 1,
+        '2': 2, 'two': 2,
+        '3': 3, 'three': 3,
+        '4': 4, 'four': 4,
+        '5': 5, 'five': 5,
+        '6': 6, 'six': 6,
+        '7': 7, 'seven': 7,
+        '8': 8, 'eight': 8,
+        '9': 9, 'nine': 9,
+    }
+    line_find_fn = line.find if dir == 'left' else line.rfind
+    return [(line_find_fn(digit), value) for digit, value in mappings.items() if line_find_fn(digit) != -1]
+
+
 with open('input.txt', 'r') as f:
-  input = f.read().splitlines()
+    input_data = f.read().splitlines()
 
-mappings = {
-  'one': 1, 'two': 2, 'three': 3,
-  'four': 4, 'five': 5, 'six': 6,
-  'seven': 7, 'eight': 8, 'nine': 9,
-  '1': 1, '2': 2, '3': 3, 
-  '4': 4, '5': 5, '6': 6, 
-  '7': 7, '8': 8, '9': 9
-}
-
-def positions_and_values(line: str, from_left: bool):
-  line_find = line.find if from_left else line.rfind
-  return [(line_find(digit), value) for digit, value in mappings.items() if line_find(digit) != -1]
-
-calibration_values = []
-for line in input:
-  first_value = sorted(positions_and_values(line, from_left=True))[0][1]
-  last_value = sorted(positions_and_values(line, from_left=False))[-1][1]
-
-  calibration_values.append(int(str(first_value) + str(last_value)))
+calibration_values = [int(
+    str(min(positions_and_values(line, dir='left'))[1]) +
+    str(max(positions_and_values(line, dir='right'))[1])
+) for line in input_data]
 
 print(sum(calibration_values))
