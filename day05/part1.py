92d4c18ed8f78ef9abecbfa26daf5795c86748b2
weibell <13382354+weibell@users.noreply.github.com>
2023-12-06 00:10:21 +0100

    Day 5

---
+++ day05/part1.py
@@ -0,0 +1,31 @@
+import re
+
+with open('input.txt', 'r') as f:
+    input_data = f.read().splitlines()
+
+maps = []
+for line in input_data[2:]:
+    if 'map' in line:
+        maps.append(dict())
+    elif line != '':
+        destination, source, length = [int(value) for value in line.split()]
+        maps[-1][range(source, source+length)] \
+            = range(destination, destination+length)
+
+
+def lookup_location(initial_value: int) -> int:
+    value = initial_value
+    for current_map in maps:
+        value = next(
+            (destination_range.start + (value - source_range.start)
+             for source_range, destination_range in current_map.items()
+             if value in source_range),
+            value  # fallback
+        )
+    return value
+
+
+seeds = [int(seed) for seed in re.findall(r'\d+', input_data[0])]
+locations = [lookup_location(seed) for seed in seeds]
+
+print(min(locations))
---
+++ day05/part2.py
@@ -0,0 +1,40 @@
+import re
+
+with open('input.txt', 'r') as f:
+    input_data = f.read().splitlines()
+
+maps = []
+for line in input_data[2:]:
+    if 'map' in line:
+        maps.append(dict())
+    elif line != '':
+        destination, source, length = [int(value) for value in line.split()]
+        maps[-1][range(source, source+length)] \
+            = range(destination, destination+length)
+
+
+def reverse_lookup_seed(location: int) -> int:
+    value = location
+    for current_map in reversed(maps):
+        value = next(
+            (source_range.start + (value - destination_range.start)
+             for source_range, destination_range in current_map.items()
+             if value in destination_range),
+            value  # fallback
+        )
+    return value
+
+
+initial_seed_data = [int(seed) for seed in re.findall(r'\d+', input_data[0])]
+seed_ranges = []
+for index in range(0, len(initial_seed_data) - 1, 2):
+    start, length = initial_seed_data[index:index+2]
+    seed_ranges.append(range(start, start+length))
+
+location = 0
+while True:
+    potential_seed = reverse_lookup_seed(location)
+    if any(potential_seed in seed_range for seed_range in seed_ranges):
+        print(location)
+        break
+    location += 1
