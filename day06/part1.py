80d101a7c93a2861adefef08223c87a5ad95a179
weibell <13382354+weibell@users.noreply.github.com>
2023-12-08 22:53:07 +0100

    Day 6

---
+++ day06/part1.py
@@ -0,0 +1,24 @@
+import math
+import re
+
+with open('input.txt', 'r') as f:
+    input_data = f.read().splitlines()
+
+race_durations = [int(ms) for ms in re.findall(r'\d+', input_data[0])]
+record_distances = [int(mm) for mm in re.findall(r'\d+', input_data[1])]
+
+
+# brute-force solution...
+def number_of_ways_to_win(race_duration: int, record_distance: int) -> int:
+    wins = 0
+    for hold_duration in range(1, race_duration):
+        speed = hold_duration
+        distance = (race_duration - hold_duration) * speed
+        if distance > record_distance:
+            wins += 1
+    return wins
+
+
+winning_options_per_race = [number_of_ways_to_win(race_duration, record_distance)
+                            for race_duration, record_distance in zip(race_durations, record_distances)]
+print(math.prod(winning_options_per_race))
---
+++ day06/part2.py
@@ -0,0 +1,22 @@
+import re
+
+with open('input.txt', 'r') as f:
+    input_data = f.read().replace(' ', '')
+
+race_duration = int(re.findall(r'\d+', input_data)[0])
+record_distance = int(re.findall(r'\d+', input_data)[1])
+
+
+# brute-force solution...
+def number_of_ways_to_win(race_duration: int, record_distance: int) -> int:
+    wins = 0
+    for hold_duration in range(1, race_duration):
+        speed = hold_duration
+        distance = (race_duration - hold_duration) * speed
+        if distance > record_distance:
+            wins += 1
+    return wins
+
+
+winning_options = number_of_ways_to_win(race_duration, record_distance)
+print(winning_options)
