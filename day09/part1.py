f12b1ed48747b04b0e2eb568a27685604fe9a3d6
weibell <13382354+weibell@users.noreply.github.com>
2023-12-09 11:03:03 +0100

    Day 9

---
+++ day09/part1.py
@@ -0,0 +1,24 @@
+import re
+
+with open('input.txt', 'r') as f:
+    input_data = f.read().splitlines()
+
+
+def sequence_differences(sequence: list[int]) -> list[int]:
+    return [sequence[i] - sequence[i - 1] for i in range(1, len(sequence))]
+
+
+extrapolated_values = []
+histories = [[int(value) for value in re.findall(r'-?\d+', history)]
+             for history in input_data]
+for history in histories:
+    sequences = [history]
+    while not all(value == 0 for value in sequences[-1]):
+        sequences.append(sequence_differences(sequences[-1]))
+
+    extrapolated_value = 0
+    for sequence in reversed(sequences):
+        extrapolated_value += sequence[-1]
+    extrapolated_values.append(extrapolated_value)
+
+print(sum(extrapolated_values))
